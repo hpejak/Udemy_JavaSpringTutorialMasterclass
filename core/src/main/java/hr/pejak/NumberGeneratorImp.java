8604a8a1636d9cc731371012438cb8f506789ea7
hpejak <0PAnT4oMiM@@>
2020-10-29 23:25:44 +0100

    Using Properties

--- core/src/main/java/hr/pejak/NumberGeneratorImp.java
+++ core/src/main/java/hr/pejak/NumberGeneratorImp.java
@@ -11,6 +11,7 @@ public class NumberGeneratorImp implements NumberGenerator{
     private final Random random = new Random();
 
     @Autowired
+    @MaxNumber
     private int maxNumber;
 
 //    Public Methods
--- core/src/main/java/hr/pejak/config/GameConfig.java
+++ core/src/main/java/hr/pejak/config/GameConfig.java
@@ -1,20 +1,29 @@
 package hr.pejak.config;
 
+import hr.pejak.GuessCount;
+import hr.pejak.MaxNumber;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
-    private int maxNumber = 100;
-    private int guessCount = 8;
+    @Value("${game.maxNumber:60}")
+    private int maxNumber;
+    @Value("${game.guessCount:7}")
+    private int guessCount;
 
     @Bean
+    @MaxNumber
     public int maxNumber(){
         return maxNumber;
     }
 
     @Bean
+    @GuessCount
     public int guessCount(){
         return guessCount;
     }
