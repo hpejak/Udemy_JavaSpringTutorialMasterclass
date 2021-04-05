349e7c4602acbe8a5cf4f896f8a4dc2eca5fa34b
hpejak <0PAnT4oMiM@@>
2021-04-05 15:49:33 +0200

    Lombok init

--- console/src/main/java/hr/pejak/console/Main.java
+++ console/src/main/java/hr/pejak/console/Main.java
@@ -1,15 +1,12 @@
 package hr.pejak.console;
 
 import hr.pejak.config.GameConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
+@Slf4j
 public class Main {
-
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         log.info("Game test");
