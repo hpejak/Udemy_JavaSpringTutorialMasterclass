7e594b6265b70bcef7a3d775c76b9ccfd27a8604
hpejak <0PAnT4oMiM@@>
2021-04-20 23:09:46 +0200

    Java Spring Tutorial Masterclass - Learn Spring Framework 5; Section 9: New Spring 5 - Spring Boot 2 And Thymeleaf 3 - Spring Boot Developer Tools; Finished

---
+++ guess-the-number/web/src/main/java/hr/pejak/util/ViewNames.java
@@ -0,0 +1,11 @@
+package hr.pejak.util;
+
+public final class ViewNames {
+
+    // constants
+    public static final String HOME = "home";
+
+    // constructor
+    private ViewNames() {
+    }
+}
